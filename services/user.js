8339d3dfafc694879ea78d821205137a88906793
Eden Ben David <edenbdv1@gmail.com>
2024-03-07 11:41:04 +0200

    feed posts by username

--- services/user.js
+++ services/user.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/user');
 const PostModel = require('../models/post');
 
+
 const getUserByUsername = async (username) => {
     return await UserModel.findOne({ username: username });
 };
@@ -46,9 +47,27 @@ const updateUser = async (username, updatedField) => { //update user username
     const user = await getUserByUsername(username);
     if (!user) return null;
 
+
+     // Check if the updatedField contains the 'username' key
+     if (updatedField.hasOwnProperty('username')) {
+
+        if (updatedField.username !== user.username) {
+            
+            user.username = updatedField.username;
+
+            // Update references to this user in the posts collection
+            await PostModel.updateMany({ createdBy: user.username }, { createdBy: updatedField.username });
+        
+
+            // Save the updated user
+            await user.save();
+        }
+    }
+
+
     // Update each property in the user object based on the request body
     Object.keys(updatedField).forEach(key => {
-        if (key !== '_id') { // Exclude _id field from being updated
+        if (key !== '_id' && key !== 'username') { // Exclude _id  and username fields from being updated
             user[key] = updatedField[key];
         }
     });
