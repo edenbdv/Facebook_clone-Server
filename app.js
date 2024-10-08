01e9a8cfd5d6d2b6dbae903ed19f7bb2f738bc67
Eden Ben David <edenbdv1@gmail.com>
2024-10-08 14:58:47 +0300

    port update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 // console.log(process.env.PORT)
 
 CONNECTION_STRING = "mongodb://localhost:27017"
-PORT = 12345
+PORT = 12346
 
 
 const connectBloomFilter = require('./client_bf');
